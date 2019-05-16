b6c7a2f5273b0b361401e25e22be8b02b368bfa0
Major <962461330@qq.com>
2019-05-16 10:27:08 +0800

    Added Actor and his inheritance class

---
+++ Classes/Constant.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef _Constant_H_
+#define _Constant_H_
+
+#define ORIGIN_ATTACK 100
+#define ORIGIN_DEFENSE 10
+#define ORIGIN_RADIUS 700
+#define ORIGIN_INTERVAL 0.2
+enum ECamp{
+	BLUE, RED, MID
+};
+
+
+#endif // !Constant_H
